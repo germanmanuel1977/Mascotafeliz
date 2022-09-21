e5cd291841223728179a58c2e97cd943e782a08c
Johanna Catalina Galindo P <johannacatalinagalindop@gmail.com>
2022-09-20 21:37:43 -0500

    cambioafiliacion

--- MVC-PHP/model/admin/index.php
+++ MVC-PHP/model/admin/index.php
@@ -49,7 +49,7 @@ if(isset($_POST['btncerrar']))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos.css">
-    <title>taller</title>
+    <title>Formularios Mascota Feliz</title>
 </head>
     <body>
         <section class="title">
