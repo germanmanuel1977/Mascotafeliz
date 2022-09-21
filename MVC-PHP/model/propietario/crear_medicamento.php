ae045cbc1f54ffb6b285c60929ad8314352a35f9
germanmanuel1977 <germanmanuell@gmail.com>
2022-09-21 15:44:41 -0500

    creación carpetas veterinario y propietario, eliminación de formularios de acuerdo al tipo de usuario

---
+++ MVC-PHP/model/propietario/crear_medicamento.php
@@ -0,0 +1,137 @@
+
+<?php
+session_start();
+require_once("../../db/connection.php");
+include("../../controller/validarSesion.php");
+$sql = "SELECT * FROM persona, tipousuario WHERE idpersona = '".$_SESSION['usuario']."' AND persona.idtipousua = tipousuario.idtipousua";
+$usuarios = mysqli_query($mysqli, $sql);
+$usua = mysqli_fetch_assoc($usuarios);
+
+
+?>
+
+<?php
+if ((isset($_POST["btnguardar"]))&&($_POST["btnguardar"]=="frmadd")){
+    $tpmedic = $_POST['medicamento'];
+    $sqladd="SELECT * FROM medicamento WHERE medicamento = '$tpmedic' ";
+    $query = mysqli_query($mysqli,$sqladd);
+    $fila = mysqli_fetch_assoc($query);
+
+if ($fila){
+    echo '<script>alert ("El Medicamento ya existe");</script>';
+    echo '<script>window.location="crear_medicamento.php"</script>';
+    
+}elseif ($_POST['medicamento']=="") {
+    echo '<script>alert ("Existen campos vacios");</script>';
+    echo '<script>window.location="crear_medicamento.php"</script>';
+
+}else{
+    $tpmedic = $_POST['medicamento'];
+    $sqladd="INSERT INTO medicamento(medicamento) VALUES ('$tpmedic') ";
+    $query = mysqli_query($mysqli,$sqladd);
+    echo '<script>alert ("Ingreso Exitoso");</script>';
+    echo '<script>window.location="crear_medicamento.php"</script>';
+
+
+}
+
+}
+?>
+
+
+<form method="POST">
+
+    <tr>
+        <td colspan='2' align="center"><?php echo $usua['nombres']?></td>
+    </tr>
+<tr><br>
+    <td colspan='2' align="center">
+    
+    
+        <input type="submit" value="Cerrar sesión" name="btncerrar" /></td>
+        <input type="submit" formaction="../admin/index.php" value="Regresar" />
+    </tr>
+</form>
+
+<?php 
+
+if(isset($_POST['btncerrar']))
+{
+	session_destroy();
+
+   
+    header('location: ../../index.html');
+}
+	
+?>
+
+</div>
+
+</div>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>taller</title>
+</head>
+    <body onload="frmadd.tipousua.focus">
+        <section class="title">
+            <h1>Formulario de creación de tipos de medicamentos    <?php echo $usua['tipousua']?></h1>
+        </section>
+
+        <table class="centrar">
+            <form method="POST" name = "frmadd" autocomplete="off">
+                
+            </tr>
+        
+                <td colspan="2">Medicamentos</td>
+                       
+             
+            </tr>
+
+            </tr>
+        
+                <td> id </td>
+                <td> <input type="text" readonly> </td>
+                       
+             
+            </tr>
+
+            </tr>
+        
+                <td> Medicamento </td>
+                <td> <input type="text" name="medicamento" placeholder="Ingrese Medicamento" style="text-transform:uppercase"> </td>
+                       
+             
+            </tr>
+            
+            </tr>
+        
+                <td colspan="2">&nbsp; </td>
+                       
+             
+            </tr>
+
+            </tr>
+        
+                <td colspan="2"><input type="submit" name="btnadd" value="Guardar"> </td>
+                <input type="hidden" name="btnguardar" value="frmadd">
+                       
+             
+            </tr>            
+
+            </form>
+
+
+        </table>
+    
+
+    </body>
+</html>
