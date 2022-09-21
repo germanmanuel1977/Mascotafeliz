bfaeaae310d90421a9b4c853936a6b83bf7248ab
Johanna Catalina Galindo P <johannacatalinagalindop@gmail.com>
2022-09-20 21:49:54 -0500

    cambiocolorformulario

--- MVC-PHP/model/admin/crear_afiliacion.php
+++ MVC-PHP/model/admin/crear_afiliacion.php
@@ -3,21 +3,19 @@
 session_start();
 require_once("../../db/connection.php");
 include("../../controller/validarSesion.php");
-$sql = "SELECT * FROM persona, tipousuario WHERE idpersona = '".$_SESSION['usuario']."' AND persona.idtipousua = tipousuario.idtipousua";
+$sql = "SELECT * FROM afiliacion, mascota WHERE idmascota = '".$_SESSION['usuario']."' AND afiliacion.idmascota = mascota.idmascota";
 $usuarios = mysqli_query($mysqli, $sql);
 $usua = mysqli_fetch_assoc($usuarios);
-
+//se direcciona también
 
 ?>
 
 <?php
-$sql = "SELECT * FROM tipousuario";
+$sql = "SELECT * FROM afiliacion";
 $tp_usu = mysqli_query($mysqli,$sql);
-$usua1 = mysqli_fetch_assoc($usuarios);
+$usua1 = mysqli_fetch_assoc($tp_usu);
 
-$sql = "SELECT * FROM estado WHERE estado < 3";
-$tp_usu2 = mysqli_query($mysqli,$sql);
-$usua2 = mysqli_fetch_assoc($usuarios);
+
 
 
 ?>
@@ -25,43 +23,36 @@ $usua2 = mysqli_fetch_assoc($usuarios);
 
 <?php
 if ((isset($_POST["btnguardar"]))&&($_POST["btnguardar"]=="frmadd")){
-    $tp = $_POST['doc'];
-    $sqladd="SELECT * FROM persona WHERE idpersona = '$tp' ";
+    $tp = $_POST['idafi'];
+    $sqladd="SELECT * FROM afiliacion WHERE idafiliacion = '$tp' ";
     $query = mysqli_query($mysqli,$sqladd);
     $fila = mysqli_fetch_assoc($query);
 
 if ($fila){
-    echo '<script>alert ("El usuario ya existe");</script>';
-    echo '<script>window.location="usuarios.php"</script>';
+    echo '<script>alert ("La afiliación ya existe");</script>';
+    echo '<script>window.location="crear_afiliacion.php"</script>';
     
-}elseif ($_POST['doc']==""||$_POST['nom']==""||$_POST['ape']==""||$_POST['dir']==""||$_POST['tel']==""||$_POST['email']==""||$_POST['password']==""||$_POST['tarjetaprof']=="")  {
+}elseif ($_POST['idafi']==""||$_POST['fechaafi']==""||$_POST['idmasc']==""){
     echo '<script>alert ("Existen campos vacios");</script>';
-    echo '<script>window.location="usuarios.php"</script>';
+    echo '<script>window.location="crear_afiliacion.php"</script>';
 
 }else{
-    $doc = $_POST['doc'];
-    $nombres = $_POST['nom'];
-    $apellidos = $_POST['ape'];
-    $direccion = $_POST['dir'];
-    $telefono = $_POST['tel'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $tarjetaprof = $_POST['tarjetaprof'];
-    $idtipousua = $_POST['idtipousua'];
-    $idestado = $_POST['idestado'];
+    $idafi = $_POST['idafi'];
+    $fechaafi = $_POST['fechaafi'];
+    $idmascota = $_POST['idmasc'];
+    
 
 
 
-
-    $sqladd="INSERT INTO persona(idpersona, nombres, apellidos, direccion, telefono, email, password, tarjetaprof, idtipousua, idestado) VALUES ('$doc','$nombres','$apellidos','$direccion','$telefono','$email','$password','$tarjetaprof','$idtipousua','$idestado')";
+    $sqladd="INSERT INTO afiliacion(idafiliacion, fechaafi, idmascota) VALUES ('$idafi','$fechaafi','$idmascota')";
     $query = mysqli_query($mysqli,$sqladd);
     echo '<script>alert ("Ingreso Exitoso");</script>';
-    echo '<script>window.location="usuarios.php"</script>';
+    echo '<script>window.location="crear_afiliacion.php"</script>';
 
 
 }
 
-}
+
 ?>
 
 
@@ -109,7 +100,7 @@ if(isset($_POST['btncerrar']))
 </head>
     <body onload="frmadd.tipousua.focus">
         <section class="title">
-            <h1>Formulario de creación de usuarios <?php echo $usua['tipousua']?></h1>
+            <h1>Formulario de afiliación <?php echo $usua['idafi']?></h1>
         </section>
 
         <table class="centrar">
@@ -117,103 +108,60 @@ if(isset($_POST['btncerrar']))
                 
             </tr>
         
-                <td colspan="2">Tipos de usuarios</td>
+                <td colspan="2">Afiliación</td>
+                       
+             
+            </tr>
+
+            </tr>
+        //autoincremental//
+                <td> Código de afiliación </td>
+                <td> <input type="number" name="idafi" placeholder="Ingrese el código de afiliación" > </td>
                        
              
             </tr>
 
             </tr>
         
-                <td> Documento de identidad </td>
-                <td> <input type="text" name="doc" placeholder="Ingrese su documento" > </td>
+                <td> Fecha afiliación </td>
+                <td> <input type="date" name="fechaafi" placeholder="Ingrese fecha de afiliación" > </td>
                        
              
             </tr>
 
             </tr>
         
-                <td> nombres </td>
-                <td> <input type="text" name="nom" placeholder="Ingrese sus nombres" style="text-transform:uppercase"> </td>
+                <td> Código mascota </td>
+                <td> <input type="number" name="idmasc" placeholder="Ingrese el código de la mascota" > </td>
                        
-             
             </tr>
-
-            </tr>
-        
-                <td> apellidos </td>
-                <td> <input type="text" name="ape" placeholder="Ingrese sus apellidos" style="text-transform:uppercase"> </td>
-                       
-             
-            </tr>
-
-            </tr>
-        
-                <td> direccion </td>
-                <td> <input type="text" name="dir" placeholder="Ingrese su direccion" style="text-transform:uppercase"> </td>
-                       
-             
-            </tr>
-
-            </tr>
-        
-                <td> telefono </td>
-                <td> <input type="number" name="tel" placeholder="Ingrese su telefono"> </td>
-                       
-             
-            </tr>
-
-            </tr>
-        
-                <td> email </td>
-                <td> <input type="email" name="email" placeholder="Ingrese su email"> </td>
-                       
-             
-            </tr>
-
-            </tr>
-        
-                <td> password </td>
-                <td> <input type="password" name="password" placeholder="Ingrese su contraseña"> </td>
-                       
-             
-            </tr>
-
-            </tr>
-        
-                <td> tarjeta profesional </td>
-                <td> <input type="text" name="tarjetaprof" placeholder="Ingrese su número de tarjeta profesional" value="0"> </td>
-                       
-             
-            </tr>
-
-            </tr>
-        
-                <td> tipo de usuario </td>
+                  
+                <td> Afiliación </td>
                 <td>
-                    <select name="idtipousua">
+                    <select name="idafiliacion">
                         <option value=""> Selecciona una opción </option>
                         <?php
                         do {
                         ?>
-                        <option value="<?php echo($usua1['idtipousua'])?>"><?php echo($usua1['tipousua'])?>
+                        <option value="<?php echo($usua1['idafiliacion'])?>"><?php echo($usua1['idafiliacion'])?></option>
                         <?php
-                        }while($usua1=mysqli_fetch_assoc($tp_usu));
+                        }while($usua1=mysqli_fetch_assoc($idafi));
                         ?>
                     </select>
                 </td>
             </tr>
 
             </tr>
-                <td> estado </td>
+                <td> Fecha Afiliación </td>
                 <td>
-                    <select name="idestado">
+                    <select name="fechaafi">
                         <option value=""> Selecciona una opción </option>
                         <?php
                         do {
                         ?>
-                        <option value="<?php echo($usua2['idestado'])?>"><?php echo($usua2['estado'])?>
+                        <option value="<?php echo($usua2['fechaafi'])?>"><?php echo($usua2['fechaafi'])?>
                         <?php
-                        }while($usua2=mysqli_fetch_assoc($tp_usu2));
+                        }while($usua2=mysqli_fetch_assoc($fechaafi));
                         ?>
                     </select>
                 </td>
