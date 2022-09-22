d72415b8414f34816809af161ddfa38da611f8bd
Johanna Catalina Galindo P <johannacatalinagalindop@gmail.com>
2022-09-21 19:56:55 -0500

    codigoafiliacion

--- MVC-PHP/model/admin/crear_afiliacion.php
+++ MVC-PHP/model/admin/crear_afiliacion.php
@@ -3,56 +3,48 @@
 session_start();
 require_once("../../db/connection.php");
 include("../../controller/validarSesion.php");
-$sql = "SELECT * FROM afiliacion, mascota WHERE idmascota = '".$_SESSION['usuario']."' AND afiliacion.idmascota = mascota.idmascota";
+$sql = "SELECT * FROM persona, tipousuario WHERE idpersona = '".$_SESSION['usuario']."' AND persona.idtipousua = tipousuario.idtipousua";
 $usuarios = mysqli_query($mysqli, $sql);
 $usua = mysqli_fetch_assoc($usuarios);
-//se direcciona también
+
 
 ?>
 
 <?php
-$sql = "SELECT * FROM afiliacion";
-$tp_usu = mysqli_query($mysqli,$sql);
-$usua1 = mysqli_fetch_assoc($tp_usu);
-
-
-
-
+$sql = "SELECT * FROM mascota ";
+$tp_usu1 = mysqli_query($mysqli,$sql);
+$usua1 = mysqli_fetch_assoc($tp_usu1);
 ?>
 
 
 <?php
 if ((isset($_POST["btnguardar"]))&&($_POST["btnguardar"]=="frmadd")){
-    $tp = $_POST['idafi'];
-    $sqladd="SELECT * FROM afiliacion WHERE idafiliacion = '$tp' ";
+    $idmascota= $_POST['idmascota'];
+    $sqladd="SELECT * FROM afiliacion WHERE idmascota = $idmascota ";
     $query = mysqli_query($mysqli,$sqladd);
     $fila = mysqli_fetch_assoc($query);
 
 if ($fila){
-    echo '<script>alert ("La afiliación ya existe");</script>';
+    echo '<script>alert ("La mascota ya está afiliada");</script>';
     echo '<script>window.location="crear_afiliacion.php"</script>';
     
-}elseif ($_POST['idafi']==""||$_POST['fechaafi']==""||$_POST['idmasc']==""){
+}elseif ($_POST['fechaafi']==""||$_POST['idmascota']=="")  {
     echo '<script>alert ("Existen campos vacios");</script>';
     echo '<script>window.location="crear_afiliacion.php"</script>';
 
 }else{
-    $idafi = $_POST['idafi'];
     $fechaafi = $_POST['fechaafi'];
-    $idmascota = $_POST['idmasc'];
-    
-
-
-
-    $sqladd="INSERT INTO afiliacion(idafiliacion, fechaafi, idmascota) VALUES ('$idafi','$fechaafi','$idmascota')";
+    $idmascota= $_POST['idmascota'];
+    $sqladd="INSERT INTO afiliacion(fechaafi, idmascota) VALUES ('$fechaafi','$idmascota')";
     $query = mysqli_query($mysqli,$sqladd);
     echo '<script>alert ("Ingreso Exitoso");</script>';
     echo '<script>window.location="crear_afiliacion.php"</script>';
 
+    
 
 }
 
-
+}
 ?>
 
 
@@ -96,11 +88,11 @@ if(isset($_POST['btncerrar']))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos.css">
-    <title>taller</title>
+    <title>Formulario Afiliación de Mascota</title>
 </head>
     <body onload="frmadd.tipousua.focus">
         <section class="title">
-            <h1>Formulario de afiliación <?php echo $usua['idafi']?></h1>
+            <h1>Afiliación de Mascota <?php echo $usua['tipousua']?></h1>
         </section>
 
         <table class="centrar">
@@ -108,67 +100,38 @@ if(isset($_POST['btncerrar']))
                 
             </tr>
         
-                <td colspan="2">Afiliación</td>
+                <td colspan="2"> A F I L I A C I Ó N</td>
                        
              
             </tr>
 
             </tr>
-        //autoincremental//
-                <td> Código de afiliación </td>
-                <td> <input type="number" name="idafi" placeholder="Ingrese el código de afiliación" > </td>
+       
+                <td> Fecha de afiliación </td>
+                <td> <input type="date" name="fechaafi" placeholder="Ingrese fecha Afiliacion"> </td>
                        
              
             </tr>
 
             </tr>
-        
-                <td> Fecha afiliación </td>
-                <td> <input type="date" name="fechaafi" placeholder="Ingrese fecha de afiliación" > </td>
-                       
-             
-            </tr>
-
-            </tr>
-        
-                <td> Código mascota </td>
-                <td> <input type="number" name="idmasc" placeholder="Ingrese el código de la mascota" > </td>
-                       
-            </tr>
-                  
-                <td> Afiliación </td>
+                          
+                <td> Mascota </td>
                 <td>
-                    <select name="idafiliacion">
+                    <select name="idmascota">
                         <option value=""> Selecciona una opción </option>
                         <?php
                         do {
                         ?>
-                        <option value="<?php echo($usua1['idafiliacion'])?>"><?php echo($usua1['idafiliacion'])?></option>
+                        <option value="<?php echo($usua1['idmascota'])?>"><?php echo($usua1['nombre'])?>
                         <?php
-                        }while($usua1=mysqli_fetch_assoc($idafi));
+                        }while($usua1=mysqli_fetch_assoc($tp_usu1));
                         ?>
                     </select>
                 </td>
             </tr>
 
             </tr>
-                <td> Fecha Afiliación </td>
-                <td>
-                    <select name="fechaafi">
-                        <option value=""> Selecciona una opción </option>
-                        <?php
-                        do {
-                        ?>
-                        <option value="<?php echo($usua2['fechaafi'])?>"><?php echo($usua2['fechaafi'])?>
-                        <?php
-                        }while($usua2=mysqli_fetch_assoc($fechaafi));
-                        ?>
-                    </select>
-                </td>
-            </tr>
-                                                                                                  
-            </tr>
-        
+                       
                 <td colspan="2">&nbsp; </td>
                        
              
